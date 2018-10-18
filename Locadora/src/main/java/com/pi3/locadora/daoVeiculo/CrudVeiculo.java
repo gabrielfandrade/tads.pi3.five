da1d4b77017be484faca67bdceae97b4ca08c4f3
Pichau <Pichau@192.168.1.101>
2018-10-17 22:48:24 -0300

    Alterações e adpatações

--- Locadora/src/main/java/com/pi3/locadora/daoVeiculo/CrudVeiculo.java
+++ Locadora/src/main/java/com/pi3/locadora/daoVeiculo/CrudVeiculo.java
@@ -21,174 +21,170 @@ import javax.swing.JOptionPane;
  * @author Pichau
  */
 public class CrudVeiculo {
-    
-    private Connection conn;
-    private ResultSet rst;
-    private PreparedStatement pst;
-    private String sql;
-    private StringBuilder result;
-    private ArrayList<Veiculo> list;
-    
-    public CrudVeiculo(){
-        setSql("");
-        
-        result = new StringBuilder();
-    }
-    
-    public void adicionarVeiculo(Veiculo v)throws SQLException{
+
+    public void adicionarVeiculo(Veiculo v) throws SQLException {
+
+        Connection conn = Connect.getConnection();
+        PreparedStatement stmt = null;
+
         try {
-            conn = Connect.getConnection();
-            
-            setSql("INSERT INTO VEICULO (MODELO, CATEGORIA, ANO, PLACA, MARCA, "
+
+            String sql = "INSERT INTO VEICULO (MODELO, CATEGORIA, ANO, PLACA, MARCA, "
                     + "NUMERODOC, CARACTERISTICAS, IMAGEM) VALUES "
-                    + "(?,?,?,?,?,?,?,?)");
-            
-            pst = conn.prepareStatement(sql);
-            
-            pst.setString(1, v.getModelo());
-            pst.setString(2, v.getCategoria());
-            pst.setDate(3, (Date) v.getAno());
-            pst.setString(4, v.getPlaca());
-            pst.setString(5, v.getMarca());
-            pst.setInt(6, v.getNumeroDoc());
-            pst.setString(7, v.getCaracter());
-            pst.setObject(8, v.getImagem());
-            
-            pst.execute();
-            
-        }catch(Exception e){
-            e.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Erro ao inserir os dados na "
-                    + "fonte de dados:\n"
-                    + e, "Erro", JOptionPane.ERROR_MESSAGE);
-        }finally{
+                    + "(?,?,?,?,?,?,?,?)";
+
+            stmt = conn.prepareStatement(sql);
+
+            stmt.setString(1, v.getModelo());
+            stmt.setString(2, v.getCategoria());
+            stmt.setDate(3, (Date) v.getAno());
+            stmt.setString(4, v.getPlaca());
+            stmt.setString(5, v.getMarca());
+            stmt.setInt(6, v.getNumeroDoc());
+            stmt.setString(7, v.getCaracter());
+            stmt.setObject(8, v.getImagem());
+
+            stmt.execute();
+
+            JOptionPane.showMessageDialog(null, "Salvo com sucesso");
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao salvar"
+                    + e);
+        } finally {
             if (conn != null && !conn.isClosed()) {
                 conn.close();
             }
         }
     }
-    
-    public ArrayList<Veiculo> listarTodosVeiculos()throws SQLException{
-      Veiculo prt = new Veiculo();
 
-        conn = Connect.getConnection();
+    public ArrayList<Veiculo> ApresentarTodosVeiculos() throws SQLException {
 
-        setSql("SELECT * FROM VEICULO");
+        Connection conn = Connect.getConnection();
 
-        pst = conn.prepareStatement(sql);
+        PreparedStatement stmt = null;
 
-        rst = pst.executeQuery();
+        ResultSet rst = null;
 
-        while (rst.next()) {
-            if (list == null) {
-                list = new ArrayList<>();
-            }
+        ArrayList<Veiculo> veiculos = new ArrayList<>();
+        
+        String sql = "SELECT * FROM VEICULO";
 
-            prt.setId(rst.getInt("IDVEICULO"));
-            prt.setModelo(rst.getString("MODELO"));
-            prt.setCategoria(rst.getString("CATEGORIA"));
-            prt.setAno(rst.getDate("ANO"));
-            prt.setPlaca(rst.getString("PLACA"));
-            prt.setMarca(rst.getString("MARCA"));
-            prt.setNumeroDoc(rst.getInt("NUMERODOC"));
-            prt.setCaracter(rst.getString("CARACTERISTICAS"));
-            prt.setImagem((File) rst.getObject("IMAGEM"));
-
-            setAddList(prt);
-
-        }
-
-        return getList();  
-    }
-    
-    public ArrayList<Veiculo> listarVeiculos(String att, String busca)throws SQLException{
-      Veiculo prt = new Veiculo();
-
-        conn = Connect.getConnection();
-
-        setSql("SELECT * FROM VEICULO WHERE (?=?)");
-
-        pst = conn.prepareStatement(sql);
-        pst.setString(1, att); 
-        pst.setString(2, busca);
-
-        rst = pst.executeQuery();
-
-        while (rst.next()) {
-            if (list == null) {
-                list = new ArrayList<>();
-            }
-
-            prt.setId(rst.getInt("IDVEICULO"));
-            prt.setModelo(rst.getString("MODELO"));
-            prt.setCategoria(rst.getString("CATEGORIA"));
-            prt.setAno(rst.getDate("ANO"));
-            prt.setPlaca(rst.getString("PLACA"));
-            prt.setMarca(rst.getString("MARCA"));
-            prt.setNumeroDoc(rst.getInt("NUMERODOC"));
-            prt.setCaracter(rst.getString("CARACTERISTICAS"));
-            prt.setImagem((File) rst.getObject("IMAGEM"));
-
-            setAddList(prt);
-
-        }
-
-        return getList();  
-    }
-    
-    public void atualizarVeiculo(Veiculo v)throws SQLException{
         try {
-            conn = Connect.getConnection();
             
-            setSql("UPDATE VEICULO SET MODELO=?, CATEGORIA=?,"
+            stmt = conn.prepareStatement(sql);
+            rst = stmt.executeQuery();
+
+            while (rst.next()) {
+                
+                Veiculo v = new Veiculo();
+
+                v.setId(rst.getInt("IDVEICULO"));
+                v.setModelo(rst.getString("MODELO"));
+                v.setCategoria(rst.getString("CATEGORIA"));
+                v.setAno(rst.getDate("ANO"));
+                v.setPlaca(rst.getString("PLACA"));
+                v.setMarca(rst.getString("MARCA"));
+                v.setNumeroDoc(rst.getInt("NUMERODOC"));
+                v.setCaracter(rst.getString("CARACTERISTICAS"));
+                v.setImagem((File) rst.getObject("IMAGEM"));
+
+                veiculos.add(v);
+
+            }
+        }catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao salvar"
+                    + e);
+        } finally {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+            }
+        }
+
+        return veiculos;
+    }
+
+    public ArrayList<Veiculo> ApresentarVeiculos(String att, String busca) throws SQLException {
+        Connection conn = Connect.getConnection();
+
+        PreparedStatement stmt = null;
+
+        ResultSet rst = null;
+
+        ArrayList<Veiculo> veiculos = new ArrayList<>();
+        
+        String sql = "SELECT * FROM VEICULO WHERE (?=?)";
+
+        try {
+            
+            stmt = conn.prepareStatement(sql);
+            
+            //tratar condições//
+            
+            rst = stmt.executeQuery();
+
+            while (rst.next()) {
+                
+                Veiculo v = new Veiculo();
+
+                v.setId(rst.getInt("IDVEICULO"));
+                v.setModelo(rst.getString("MODELO"));
+                v.setCategoria(rst.getString("CATEGORIA"));
+                v.setAno(rst.getDate("ANO"));
+                v.setPlaca(rst.getString("PLACA"));
+                v.setMarca(rst.getString("MARCA"));
+                v.setNumeroDoc(rst.getInt("NUMERODOC"));
+                v.setCaracter(rst.getString("CARACTERISTICAS"));
+                v.setImagem((File) rst.getObject("IMAGEM"));
+
+                veiculos.add(v);
+
+            }
+        }catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao salvar"
+                    + e);
+        } finally {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+            }
+        }
+
+        return veiculos;
+    }
+
+    public void atualizarVeiculo(Veiculo v) throws SQLException {
+        String sql = "UPDATE VEICULO SET MODELO=?, CATEGORIA=?,"
                     + " ANO=?, PLACA=?, MARCA=?, "
                     + "NUMERODOC=?, CARACTERISTICAS=?, IMAGEM=? "
-                    + "WHERE (IDVEICULO=?) ");
-            
-            pst = conn.prepareStatement(sql);
-            
-            pst.setString(1, v.getModelo());
-            pst.setString(2, v.getCategoria());
-            pst.setDate(3, (Date) v.getAno());
-            pst.setString(4, v.getPlaca());
-            pst.setString(5, v.getMarca());
-            pst.setInt(6, v.getNumeroDoc());
-            pst.setString(7, v.getCaracter());
-            pst.setObject(8, v.getImagem());
-            pst.setInt(9, v.getId());
-            
-            pst.execute();
-            
-        }catch(Exception e){
-            e.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Erro ao inserir os dados na "
-                    + "fonte de dados:\n"
-                    + e, "Erro", JOptionPane.ERROR_MESSAGE);
-        }finally{
+                    + "WHERE (IDVEICULO=?)";
+        Connection conn = Connect.getConnection();
+        PreparedStatement stmt = null;
+
+        try {
+
+            stmt = conn.prepareStatement(sql);
+
+            stmt.setString(1, v.getModelo());
+            stmt.setString(2, v.getCategoria());
+            stmt.setDate(3, (Date) v.getAno());
+            stmt.setString(4, v.getPlaca());
+            stmt.setString(5, v.getMarca());
+            stmt.setInt(6, v.getNumeroDoc());
+            stmt.setString(7, v.getCaracter());
+            stmt.setObject(8, v.getImagem());
+            stmt.setInt(9, v.getId());
+
+            stmt.execute();
+
+            JOptionPane.showMessageDialog(null, "Salvo com sucesso");
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao salvar"
+                    + e);
+        } finally {
             if (conn != null && !conn.isClosed()) {
                 conn.close();
             }
         }
     }
-    
-    public String getSql() {
-        return sql;
-    }
-
-    public void setSql(String sql) {
-        this.sql = sql;
-    }
-
-    public ArrayList<Veiculo> getList() {
-        return this.list;
-    }
-
-    public void setList(ArrayList<Veiculo> list) {
-        this.list = list;
-    }
-
-    public void setAddList(Veiculo prt) {
-        this.list.add(prt);
-    }
-    
 }
